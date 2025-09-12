b19dc85825b4c899eaac91d87c78b8dfe9684d0b
legoChoi <chlx159@gmail.com>
2025-09-12 18:02:23 +0900

    feat: 전시회 수정 API 용 DTO 추가

---
+++ src/main/java/com/dayaeyak/exhibition/common/constraints/ExhibitionValidationMessage.java
@@ -0,0 +1,40 @@
+package com.dayaeyak.exhibition.common.constraints;
+
+public class ExhibitionValidationMessage {
+
+    private ExhibitionValidationMessage() {}
+
+    // name
+    public static final String INVALID_NAME = "올바른 전시회 명을 입력 해주세요.";
+
+    // place
+    public static final String INVALID_PLACE = "올바른 장소 명을 입력 해주세요.";
+
+    // address
+    public static final String INVALID_ADDRESS = "올바른 주소를 입력 해주세요.";
+
+    // region
+    public static final String INVALID_REGION = "올바른 지역 값을 입력해주세요.";
+
+    // grade
+    public static final String INVALID_GRADE = "올바른 등급 값을 입력해주세요.";
+
+    // StartDate
+    public static final String INVALID_START_DATE = "올바른 시작 날짜를 입력해주세요.";
+
+    // endDate
+    public static final String INVALID_END_DATE = "올바른 종료 날짜를 입력해주세요.";
+
+    // startTime
+    public static final String INVALID_START_TIME = "올바른 시작 시간을 입력 해주세요.";
+
+    // endTime
+    public static final String INVALID_END_TIME = "올바른 종료 시간을 입력 해주세요.";
+
+    // ticketOpenAt
+    public static final String INVALID_TICKET_OPEN_AT = "올바른 티켓 판매 시작 날짜를 입력 해주세요.";
+
+    // ticketCloseAt
+    public static final String INVALID_TICKET_CLOSE_AT = "올바른 티켓 판매 종료 날짜를 입력 해주세요.";
+
+}
