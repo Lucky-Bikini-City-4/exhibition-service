6943634d7580c26fb71a15b7c4423817ff7bfb77
legoChoi <chlx159@gmail.com>
2025-09-17 16:22:40 +0900

    refactor: Passport 인증 유저 정보 관련 코드 수정

--- src/main/java/com/dayaeyak/exhibition/common/resolver/PassportHolderArgumentResolver.java
+++ src/main/java/com/dayaeyak/exhibition/common/resolver/PassportHolderArgumentResolver.java
@@ -3,17 +3,18 @@ package com.dayaeyak.exhibition.common.resolver;
 import com.dayaeyak.exhibition.common.annotation.PassportHolder;
 import com.dayaeyak.exhibition.common.dto.Passport;
 import com.dayaeyak.exhibition.common.enums.UserRole;
+import com.dayaeyak.exhibition.common.exception.CustomRuntimeException;
+import com.dayaeyak.exhibition.common.exception.type.CommonExceptionType;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PassportHolderArgumentResolver implements HandlerMethodArgumentResolver {
@@ -33,10 +34,18 @@ public class PassportHolderArgumentResolver implements HandlerMethodArgumentReso
             ModelAndViewContainer mavContainer,
             NativeWebRequest webRequest,
             WebDataBinderFactory binderFactory
-    ) throws Exception {
+    ) {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
         String id = request.getHeader(USER_ID_HEADER);
         String role = request.getHeader(USER_ROLE_HEADER);
+
+        if (!StringUtils.hasText(id)) {
+            throw new CustomRuntimeException(CommonExceptionType.INVALID_USER_ID);
+        }
+
+        if (!StringUtils.hasText(role)) {
+            throw new CustomRuntimeException(CommonExceptionType.INVALID_USER_ROLE);
+        }
 
         return new Passport(
                 Long.valueOf(id),
